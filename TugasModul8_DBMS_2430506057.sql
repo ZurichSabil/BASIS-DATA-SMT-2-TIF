4de6b4bf4a451e52bca2dcd048d9efe4025ad7cc
Zurich Sabil <zurichsabil@gmail.com>
2025-05-27 02:40:19 +0700

    Add files via upload

---
+++ TugasModul8_DBMS_2430506057.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Bulan Mei 2025 pada 21.38
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kampus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `nip` char(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `bidang` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `dosen`
+--
+
+INSERT INTO `dosen` (`nip`, `nama`, `bidang`) VALUES
+('D001', 'Rina Suryani', 'Basis Data'),
+('D002', 'Prof. Ari Hidayat', 'Jaringan'),
+('D003', 'Ir. Azkal Rahma', 'Pemrograman');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` char(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jurusan` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jurusan`) VALUES
+('M001', 'Teguh Gintar', 'Teknik Informatika'),
+('M002', 'Budi Arya', 'Sistem Informasi'),
+('M003', 'Azkal afan', 'Teknik Informatika'),
+('M009', 'Taufik', 'Gizi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `kode_mk` char(6) NOT NULL,
+  `nama_mk` varchar(50) DEFAULT NULL,
+  `sks` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`kode_mk`, `nama_mk`, `sks`) VALUES
+('MK001', 'Basis Data', 3),
+('MK002', 'Algoritma dan Pemrograman', 4),
+('MK003', 'Jaringan Komputer', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tagihan`
+--
+
+CREATE TABLE `tagihan` (
+  `id_tagihan` int(11) NOT NULL,
+  `nim` char(10) DEFAULT NULL,
+  `jenis_tagihan` varchar(30) DEFAULT NULL,
+  `jumlah_tagihan` decimal(10,2) DEFAULT NULL,
+  `status` enum('Lunas','Belum Lunas') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tagihan`
+--
+
+INSERT INTO `tagihan` (`id_tagihan`, `nim`, `jenis_tagihan`, `jumlah_tagihan`, `status`) VALUES
+(1, 'M001', 'UKT', 1000000.00, 'Lunas'),
+(2, 'M002', 'UKT', 1500000.00, 'Lunas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `temp_table`
+--
+
+CREATE TABLE `temp_table` (
+  `id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indeks untuk tabel `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`kode_mk`);
+
+--
+-- Indeks untuk tabel `tagihan`
+--
+ALTER TABLE `tagihan`
+  ADD PRIMARY KEY (`id_tagihan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
